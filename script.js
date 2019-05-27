e361a91eb9770bc11dcdff41ffc63b49344ea0e1
cis101 <cis101@secs.iub.edu.bd>
2019-05-27 09:32:21 +0600

    task-2

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 /* JavaScript code here */
 console.log("External javascript");
+
+
+
+//task 2: Summation of array
+
+function sum(arr){
+
+    var sum=0;
+    arr.forEach(function(item,index) {
+    sum=sum+item;
+})
+
+return sum;
+}
+
+console.log(sum([1,2,3,6]))
+
+//Task 3 email address  checking
+
+function validateEmail(email){
+
+   return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+}
+
+
+var checkEmail=validateEmail('aafsj')
+console.log(checkEmail)
+console.log(validateEmail('anf@gmail.com'))
