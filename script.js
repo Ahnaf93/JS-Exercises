e609c0f8ea89bb329fd4c2fa32bd82bd61f19dd5
cis101 <cis101@secs.iub.edu.bd>
2019-05-29 08:49:43 +0600

    TASK-4

--- script.js
+++ script.js
@@ -29,3 +29,64 @@ function validateEmail(email){
  var checkEmail=validateEmail('aafsj')
  console.log(checkEmail)
  console.log(validateEmail('anf@gmail.com'))
+
+
+ //OBJECT Example//
+
+ var company = {
+    name: 'Google', 
+    age: 19, 
+    ceo: {
+    first_name: 'Sundar', 
+    last_name: 'Pichai'
+    }
+    };
+
+
+    
+    company.updateAge=function(age){
+        return age;
+    }
+
+    company.updateAge=(23);
+
+    delete company.name
+
+    console.log(company)
+
+
+//TASK-4: Access object property at a specific index of array//
+
+    var library = [ 
+        {
+            author: 'Bill Gates',
+            title: 'The Road Ahead',
+            readingStatus: true
+        },
+        {
+            author: 'Steve Jobs',
+            title: 'Walter Isaacson',
+            readingStatus: true
+        },
+        {
+            author: 'Suzanne Collins',
+            title:  'Mockingjay: The Final Book of The Hunger Games', 
+            readingStatus: false
+        }];
+
+        
+        //to print the reading status of any given index//
+        
+        function getreadingStatus(index) {
+            return library[index].readingStatus
+          }
+          console.log(getreadingStatus(2))
+
+
+          function getproperty(index, property) {
+            return library[index][property]
+          }
+
+
+          console.log(getreadingStatus(2, 'author'))
+     
